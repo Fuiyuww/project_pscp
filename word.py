eb03259376a01e5e090ec124eca0e47e74a648a5
Fuiyuww <164621072+Fuiyuww@users.noreply.github.com>
2025-10-04 00:13:59 +0700

    word.py
    mixed music and instrument together, removed some words

--- word.py
+++ word.py
@@ -1,16 +1,15 @@
 WORDS_BY_LEVEL = {
     "animals": ["PANDA", "SHEEP", "HORSE", "GOOSE", "EAGLE", "ZEBRA", "WHALE", "HIPPO", "RHINO", "SNAIL", "HYENA", "SHARK", "SLOTH"],
     "fruits": ["APPLE", "MANGO", "GRAPE", "LEMON", "PEACH", "MELON", "OLIVE", "BERRY", "GUAVA", "PLUMS"],
-    "objects": ["TABLE", "CHAIR", "PHONE", "CLOCK", "KNIFE", "GLASS", "PLATE", "RADIO", "SPOON", "BRUSH", "PAPER", "SOFAS"],
-    "instruments": ["AUDIO", "PIANO", "MUSIC", "SOUND", "DRUMS", "STAGE", "DANCE", "ALBUM", "RHYME", "REMIX", "MIXER", "OPERA", "BANDS"],
+    "objects": ["TABLE", "CHAIR", "PHONE", "CLOCK", "KNIFE", "PLATE", "RADIO", "SPOON", "BRUSH", "PAPER", "SOFAS"],
     "colors": ["GREEN", "BLACK", "BROWN", "WHITE", "CREAM", "LEMON", "PEACH", "BEIGE", "AZURE", "AMBER"],
     "body_parts": ["HEART", "BRAIN", "TOOTH", "KNEES", "ELBOW", "THUMB", "WRIST", "ANKLE", "LIVER", "SPINE"],
     "food": ["PIZZA", "CANDY", "SALAD", "HONEY", "STEAK", "CREAM", "JUICE", "SAUCE", "PASTA", "TOAST", "SUGAR", "WATER", "BREAD"],
-    "countries": ["JAPAN", "CHINA", "EGYPT", "SPAIN", "KOREA", "NEPAL", "CHILE", "ITALY", "WALES", "QATAR"],
+    "countries": ["JAPAN", "CHINA", "EGYPT", "SPAIN", "KOREA", "NEPAL", "CHILE", "ITALY"],
     "jobs": ["NURSE", "PILOT", "GUARD", "MINER", "ACTOR", "AGENT", "COACH"],
     "sports": ["CHESS", "RUGBY", "SKATE", "DARTS", "RODEO", "BOXING"],
-    "nature": ["RIVER", "OCEAN", "BEACH", "STORM", "CLOUD", "STONE", "RAINY", "WINDS", "ROCKS", "MOUNT", "FLORA"],
+    "nature": ["RIVER", "OCEAN", "BEACH", "STORM", "CLOUD", "STONE", "RAINY", "WINDS", "ROCKS", "MOUNT"],
     "time": ["MONTH", "HOURS", "EARLY", "NIGHT", "DATES", "EVENT", "LATER"],
     "emotions": ["HAPPY", "ANGRY", "PROUD", "WORRY", "SORRY", "SHOCK", "GLORY"],
-    "music": ["PIANO", "CELLO", "FLUTE", "DRUMS", "VOICE", "CHORD", "NOTES", "LYRIC", "SCALE", "OPERA"]
+    "music": ["PIANO", "CELLO", "FLUTE", "DRUMS", "VOICE", "CHORD", "NOTES", "LYRIC", "OPERA", "AUDIO", "PIANO", "SOUND", "DANCE", "ALBUM", "RHYME", "BANDS"]
 }
